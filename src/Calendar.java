ad416a459a97bf8cecf1e973c6aa4006c5ea9e31
manikandan2k29 <manikandanthangapandiyan29@gmail.com>
2021-08-19 10:50:06 +0530

    main

--- src/Calendar.java
+++ src/Calendar.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 public class Calendar {
 	
 	static void calendar(int month,int day,int year) {
+		//print the day using LocaleDate with a function getDayOfWeek()
 		System.out.println(LocalDate.of(year,month,day).getDayOfWeek().name());
 		
 	}
