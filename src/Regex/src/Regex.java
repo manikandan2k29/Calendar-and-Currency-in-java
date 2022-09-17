9ac7692f75aa2d7847732893fa705305536e5182
manikandanthangapandiyan <manikandan.thangapandiyan@betteranalytics.in>
2022-09-17 10:33:44 +0530

    test for commit

--- src/Regex/src/Regex.java
+++ src/Regex/src/Regex.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Regex {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
-		
 		while (scan.hasNext()) {
 			String IP = scan.next();
 			System.out.println(IP.matches(new MyRegex().pattern));
